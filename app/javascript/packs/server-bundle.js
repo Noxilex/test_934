dbe5e457e23d47f3cbde00b2269451edef28ee31
Noxilex <dhoop59@hotmail.fr>
2024-07-29 20:53:42 +0200

    Removed references to hello world

--- app/javascript/packs/server-bundle.js
+++ app/javascript/packs/server-bundle.js
@@ -1,8 +1,7 @@
 import ReactOnRails from 'react-on-rails';
 
-import Pizzerias from '../bundles/HelloWorld/components/Pizzerias';
+import Pizzerias from '../bundles/Pizzerias/components/Pizzerias';
 
-// This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   Pizzerias
 });
